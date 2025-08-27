54387eaed693ed27fc52fe4e1a07924ca23941aa
arman <solaa8239@gmail.com>
2025-08-27 16:13:20 +0330

    fix : syntax error fixed

--- src/lib/services/reports.ts
+++ src/lib/services/reports.ts
@@ -1,8 +1,8 @@
 import { supabase } from "../utils/db";
 
-import type { AddRemportBodyType } from "@/types";
+import type { AddReportBodyType } from "@/types";
 
-export const addReport = async (data: AddRemportBodyType): Promise<boolean> => {
+export const addReport = async (data: AddReportBodyType): Promise<boolean> => {
   await supabase.from("reports").insert(data);
   return true;
 };
